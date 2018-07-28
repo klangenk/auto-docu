6cfcaf6ce7277b73046b1aaeae6bbbbf1a11b0c1
Kevin Langenkämper <k.langenk@gmail.com>
2018-07-28 12:46:13 +0200

    added support for extension of old docs

---
+++ test/runs/withDoc.js
@@ -0,0 +1,6 @@
+module.exports = (test) => {
+  test.test1('test',5)
+  test.test2('')
+  test.test3('foo')
+  test-test4('bar')
+}
---
+++ test/snapshots/withDoc.js
@@ -0,0 +1,39 @@
+/**
+ * Awesome description
+ * @param {string} a first parameter
+ * @param {number} b second parameter
+ */
+function test1 (a, b) {
+
+}
+
+/**
+ * Awesome description 2
+ * @param {string} a first parameter
+ */
+function test2 (a) {
+
+}
+
+/**
+ * Awesome description 3
+ * @param {string} a
+ */
+function test3 (a) {
+
+}
+
+/**
+ * Awesome description 4
+ * @param {string} a
+ */
+function test4 (a) {
+
+}
+
+module.exports = {
+  test1,
+  test2,
+  test3,
+  test4
+}
